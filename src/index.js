644039d570d145be852aca840ef028b1f77327e8
aacassandra <alauddinafifcassandra@programmer.net>
2020-01-16 16:05:29 +0700

    update all

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ const Index = (Parse, request, callback) => {
     const User = new Parse.User();
     const query = new Parse.Query(User);
 
-    if (options.include) {
+    if (options && options.include) {
       query.include(options.include);
     }
 
     query.get(objectId).then(
       async object => {
-        await Tools.objectSet(object, data);
+        await Tools.objectSet(Parse, object, data);
         object.save(null, { useMasterKey: true }).then(
           async success => {
             let newSuccess = success;
@@ -53,13 +53,13 @@ const Index = (Parse, request, callback) => {
     const Class = Parse.Object.extend(className);
     const query = new Parse.Query(Class);
 
-    if (options.include) {
+    if (options && options.include) {
       query.include(options.include);
     }
 
     query.get(objectId).then(
       async object => {
-        await Tools.objectSet(object, data);
+        await Tools.objectSet(Parse, object, data);
         object.save(null, { useMasterKey: true }).then(
           async success => {
             let newSuccess = success;
@@ -78,6 +78,84 @@ const Index = (Parse, request, callback) => {
             callback(res);
           }
         );
+      },
+      error => {
+        const res = {
+          output: error,
+          status: false
+        };
+        callback(res);
+      }
+    );
+  } else if (functionName === 'ParseRetrieve') {
+    const { objectId } = request.params;
+    const { className } = request.params;
+    const options = request.params.options ? request.params.options : null;
+
+    const Class = Parse.Object.extend(className);
+    const query = new Parse.Query(Class);
+
+    if (options && options.where && Object.keys(options.where).length) {
+      Tools.objectConditional(Parse, query, options.where);
+    }
+
+    if (options && options.include) {
+      query.include(options.include);
+    }
+
+    query.get(objectId, { useMasterKey: true }).then(
+      async success => {
+        let newSuccess = success;
+        newSuccess = await Tools.attributesRemover(newSuccess);
+        if (options && options.relation) {
+          const promises = options.relation.map(async columnName => {
+            const relation = await Tools.getRelation(success, columnName);
+            Object.keys(newSuccess).forEach(column => {
+              if (column === columnName) {
+                newSuccess[column] = relation;
+              }
+            });
+          });
+          await Promise.all(promises);
+        }
+        const res = {
+          output: newSuccess,
+          status: true
+        };
+        callback(res);
+      },
+      error => {
+        const res = {
+          output: error,
+          status: false
+        };
+        callback(res);
+      }
+    );
+  } else if (functionName === 'ParseRetrieves') {
+    const { className } = request.params;
+    const options = request.params.options ? request.params.options : null;
+
+    const Class = Parse.Object.extend(className);
+    const query = new Parse.Query(Class);
+
+    if (options && options.where && Object.keys(options.where).length) {
+      Tools.objectConditional(Parse, query, options.where);
+    }
+
+    if (options && options.include) {
+      query.include(options.include);
+    }
+
+    query.find({ useMasterKey: true }).then(
+      async success => {
+        let newSuccess = success;
+        newSuccess = await Tools.attributesRemover(newSuccess);
+        const res = {
+          output: newSuccess,
+          status: true
+        };
+        callback(res);
       },
       error => {
         const res = {
--- src/tools/index.js
+++ src/tools/index.js
@@ -44,10 +44,65 @@ module.exports = {
 
     return object;
   },
+  objectConditional: (Parse, query, options) => {
+    Object.keys(options).forEach(key => {
+      if (key === 'equalTo') {
+        query.equalTo(options[key].object, options[key].value);
+      } else if (key === 'equalToPointer') {
+        query.equalTo(
+          options[key].object,
+          new Parse.Object(options[key].class, {
+            id: options[key].value
+          })
+        );
+      } else if (key === 'notEqualTo') {
+        query.notEqualTo(options[key].object, options[key].value);
+      } else if (key === 'notEqualToPointer') {
+        query.notEqualToPointer(
+          options[key].object,
+          new Parse.Object(options[key].class, {
+            id: options[key].value
+          })
+        );
+      } else if (key === 'containedIn') {
+        query.containedIn(options[key].object, options[key].value);
+      } else if (key === 'notContainedIn') {
+        query.notContainedIn(options[key].object, options[key].value);
+      } else if (key === 'greaterThan') {
+        query.greaterThan(options[key].object, options[key].value);
+      } else if (key === 'lessThan') {
+        query.lessThan(options[key].object, options[key].value);
+      } else if (key === 'greaterThanOrEqualTo') {
+        query.greaterThanOrEqualTo(options[key].object, options[key].value);
+      } else if (key === 'lessThanOrEqualTo') {
+        query.lessThanOrEqualTo(options[key].object, options[key].value);
+      }
+    });
+
+    return query;
+  },
   attributesRemover(response) {
     let newResponse = response;
     newResponse = JSON.stringify(newResponse);
     newResponse = JSON.parse(newResponse);
     return newResponse;
+  },
+  getRelation(query, columnName) {
+    return new Promise(resolve => {
+      const relation = query.relation(columnName);
+      const relationQuery = relation.query();
+      relationQuery.find({ useMasterKey: true }).then(
+        success => {
+          const response = [];
+          success.forEach(object => {
+            response.push(object.toJSON());
+          });
+          resolve(response);
+        },
+        error => {
+          resolve(error);
+        }
+      );
+    });
   }
 };
